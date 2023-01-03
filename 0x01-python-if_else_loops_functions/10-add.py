8ab2cf1c081b84eff046d7c999869dc4d6c604c2
DagogoCharles <dagogo.charles.dc@gmail.com>
2023-01-03 10:42:12 -0800

    all tasks

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author - Dagogo Charles
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
+
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author -Dagogo Charles
+
+def pow(a, b):
+    return (a ** b)
+
